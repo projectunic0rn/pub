b0df1ed4294514eeca2496c7bbdb2b1e11d9acad
Rodger Jordas <rmjordas@gmail.com>
2019-02-18 09:44:58 +0800

    Add slugify string utility function

--- src/utils/string-utils.ts
+++ src/utils/string-utils.ts
@@ -18,3 +18,14 @@ export function truncate(
 
   return text;
 }
+
+export function slugify(text: string) {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/[^\w-]+/g, '') // Remove all non-word chars
+    .replace(/--+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, ''); // Trim - from end of text
+}
