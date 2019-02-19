934fdc1ea0b45bfcd1a8ab6321399cf0f26ef160
Rodger Jordas <rmjordas@gmail.com>
2019-02-19 13:28:20 +0800

    Add Typography.js configuration file

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/blog`,
