38c6b9e5f01ea2e1616ba7b729a831baf945fa83
Rodger Jordas <rmjordas@gmail.com>
2019-03-15 03:22:59 +0800

    Update children of Layout's Helmet component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -25,9 +25,15 @@ const Layout: React.FunctionComponent = ({ children }) => {
   return (
     <Root>
       <Helmet>
+        <html lang="en" />
         <title>{siteMetadata.title}</title>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
+        />
+
         <link rel="icon" href={favicon} />
       </Helmet>
 
