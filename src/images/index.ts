6a53f151bfb641f11d4465df74f2008560f61c14
Rodger Jordas <rmjordas@gmail.com>
2019-03-22 04:02:58 +0800

    Add barrel pattern to images directory

---
+++ src/images/index.ts
@@ -0,0 +1,10 @@
+export { default as defaultAvatarImage } from './default-avatar-image.png';
+export { default as defaultPostImage } from './default-post-image.jpg';
+export { default as facebookIcon } from './facebook.svg';
+export { default as githubIcon } from './github.svg';
+export { default as instagramIcon } from './instagram.svg';
+export { default as linkedinIcon } from './linkedin.svg';
+export { default as puLogo } from './pu.svg';
+export { default as redditIcon } from './reddit.svg';
+export { default as twitterIcon } from './twitter.svg';
+export { default as unicornIcon } from './unicorn-icon.png';
