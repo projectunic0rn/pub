c1bf4686980738f08625946bf4acaabdb4a5d976
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-30 07:16:40 +0000

    Deploy projectunic0rn/pub to github.com/projectunic0rn/pub.git:gh-pages

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6fe7c41949a43fcc2d63.js"
+    "url": "webpack-runtime-84cd94b6f4e395ccc78d.js"
   },
   {
     "url": "styles.9cfa7bbfcdd73b95430d.css"
@@ -42,13 +42,13 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5ed131c8aa6aba33e01aa76b6565d3ad"
+    "revision": "fffeb462c3f92d54df7e41df734a875b"
   },
   {
     "url": "component---src-pages-404-tsx-4378f2ad0c5f47cf67aa.js"
   },
   {
-    "url": "1-8e2d6bde89dcdf18d437.js"
+    "url": "1-2575f36328cd382adf00.js"
   },
   {
     "url": "0-fed21cfb4e9d974bc90c.js"
