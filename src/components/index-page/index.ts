00f979c0faf15dcdcd99ca9c01b54a5f995fa3fe
Rodger Jordas <rmjordas@gmail.com>
2019-05-02 06:04:28 +0800

    Remove IndexPageCard component export

--- src/components/index-page/index.ts
+++ src/components/index-page/index.ts
@@ -1,4 +1,3 @@
 export { default as IndexPageHero } from './hero';
 export { default as IndexPageAbout } from './about';
 export { default as IndexPageSignup } from './signup';
-export { default as IndexPageCard } from './card';
