a7bad939af934a85d168904fd8da9c1d63f01eb7
Rodger Jordas <rmjordas@gmail.com>
2019-05-04 01:40:05 +0800

    Refactor index page Signup component

--- src/components/index-page/signup.tsx
+++ src/components/index-page/signup.tsx
@@ -1,74 +1,69 @@
 import * as React from 'react';
 
 import CtaButton from './cta-button';
-import styled, { css } from '@styled-components';
-
-const pStyles = css`
-  font-size: 1.1em;
-  margin: 25px 0;
-`;
+import styled from '@styled-components';
 
 const Wrapper = styled.section`
-  display: flex;
   align-items: center;
-  padding: 75px 55px;
-  /* background: hsl(238, 22%, 44%); */
-  background: #5f8ddc;
-  color: #fff;
+  background: ${({ theme }) => theme.colors.highlight};
+  color: ${({ theme }) => theme.colors.baseinvert};
+  display: flex;
   justify-content: space-between;
+  padding: ${({ theme }) => theme.boxes.padding.section.medium};
 
-  @media screen and (max-width: 975px) {
+  @media screen and (max-width: ${({ theme }) => theme.sizes.width.medium}) {
     flex-direction: column;
     text-align: center;
   }
 
-  @media screen and (max-width: 41.6875em) {
-    padding: 45px 25px;
+  @media screen and (max-width: ${({ theme }) => theme.sizes.width.small}) {
+    padding: ${({ theme }) => theme.boxes.padding.section.small};
   }
 `;
 
-const SignupText = styled.div`
+const Text = styled.div`
   flex: 0 0 40%;
 
-  @media screen and (max-width: 41.6875em) {
+  @media screen and (max-width: ${({ theme }) => theme.sizes.width.small}) {
     flex: 0 0 100%;
   }
 `;
 
-const SignupTextHeading = styled.h2`
+const TextHeading = styled.h2`
+  color: ${({ theme }) => theme.colors.baseinvert};
   margin: 0;
-  color: #fff;
 `;
 
-const SignupTextP = styled.p`
-  ${pStyles};
-  margin-bottom: 0;
+const TextSubHeading = styled.p`
+  font-size: 1.1em;
+  margin: 1.5625em 0 0;
 
-  @media screen and (max-width: 975px) {
-    ${pStyles};
+  @media screen and (max-width: ${({ theme }) => theme.sizes.width.medium}) {
+    margin: 1.5625em 0;
   }
 `;
 
-const SignupForm = styled.div`
-  @media screen and (max-width: 975px) {
+const Form = styled.div`
+  @media screen and (max-width: ${({ theme }) => theme.sizes.width.medium}) {
     width: 100%;
   }
 `;
 
+/** Sign up contains the last sales pitch and a call-to-action button. */
 const Signup: React.FC = () => (
   <Wrapper>
-    <SignupText>
-      <SignupTextHeading>Join our Slack group</SignupTextHeading>
+    <Text>
+      <TextHeading>Join our Slack group</TextHeading>
 
-      <SignupTextP>
+      <TextSubHeading>
         It only takes a minute to sign in to your slack account. If you have any
         questions, our members would be happy to help you.
-      </SignupTextP>
-    </SignupText>
+      </TextSubHeading>
+    </Text>
 
-    <SignupForm>
+    <Form>
       <CtaButton variant="secondary" />
-    </SignupForm>
+    </Form>
   </Wrapper>
 );
 
