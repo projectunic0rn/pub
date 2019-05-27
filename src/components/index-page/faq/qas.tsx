8302aa51543844d9512939b90851777e38070217
Rodger Jordas <rmjordas@gmail.com>
2019-05-27 11:09:49 +0800

    Finalize question and answer list

--- src/components/index-page/faq/qas.tsx
+++ src/components/index-page/faq/qas.tsx
@@ -9,40 +9,14 @@ export const qas: readonly Qa[] = [
   {
     question: 'Why the name Project Unicorn?',
     answer: (
-      <React.Fragment>
-        <p>
-          The name is inspired by the term unicorn developer/programmer. A
-          developer is sometimes referred to as a unicorn when they hold a
-          diverse range of skills that are incredibly valuable and rare. The
-          projects we work on offer an opportunity to glean into other skills
-          you might not have - including devops, design, frontend, backend, or
-          other skills required to build and ship software.
-        </p>
-      </React.Fragment>
-    ),
-  },
-  {
-    question: (
-      <>
-        Sed libero odio, volutpat <code>et faucibus euismod</code>, maximus a
-        diam?
-      </>
-    ),
-    answer: (
-      <>
-        <p>
-          Aliquam est ex, <code>finibus vitae sodales</code> eget, ultricies
-          vitae neque. Donec condimentum mollis ex, a tincidunt risus tincidunt
-          nec:
-        </p>
-        <p>
-          <ul>
-            <li>Donec lacinia eros nisi</li>
-            <li>Sit amet volutpat metus fermentum non</li>
-            <li>Sed vel fermentum augue</li>
-          </ul>
-        </p>
-      </>
+      <p>
+        The name is inspired by the term <em>unicorn developer/programmer</em>.
+        A developer is sometimes referred to as a unicorn when they hold a
+        diverse range of skills that are incredibly valuable and rare. The
+        projects we work on offer an opportunity to glean into other skills you
+        might not have - including devops, design, frontend, backend, or other
+        skills required to build and ship software.
+      </p>
     ),
   },
 ];
