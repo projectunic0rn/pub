98378075225445b04247be5abf19bbca958112e0
Rodger Jordas <rmjordas@gmail.com>
2019-06-08 16:38:11 +0800

    Add inline documentation for Content interface

--- src/components/index-page/testimonial/content.tsx
+++ src/components/index-page/testimonial/content.tsx
@@ -3,9 +3,19 @@ import * as React from 'react';
 import { avatarRmjordas } from '../assets';
 
 export interface Content {
+  /**
+   * The quote can either be a plain string or a React node. Use a React node to
+   * be able to use `em`, `strong`, etc..
+   */
   readonly quote: string | React.ReactNode;
+  /** Name of the author of the quote. */
   readonly author: string;
+  /** Title to be displayed below the author's name. */
   readonly title: string;
+  /**
+   * The image associated with the author of the quote. If not provided, a
+   * default image will be used.
+   */
   readonly avatar?: string;
 }
 
