293dd081a6a9fe3817f0aa8e75782b84a1b6ef0a
Rodger Jordas <rmjordas@gmail.com>
2019-06-28 18:13:02 +0800

    Fix title element value on IndexPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,25 @@
 import * as React from 'react';
+import Helmet from 'react-helmet';
 
 import { Hero, About, Signup, Faq, Testimonial } from '@components/index-page';
 import { Layout } from '@components/shared';
+import { useSiteMetadata } from '@hooks';
 
 /** The website's landing page. */
-const IndexPage: React.FC = () => (
-  <Layout>
-    <h1 className="visually-hidden">Welcome</h1>
-    <Hero />
-    <About />
-    <Testimonial />
-    <Faq />
-    <Signup />
-  </Layout>
-);
+const IndexPage: React.FC = () => {
+  const siteMetadata = useSiteMetadata();
+
+  return (
+    <Layout>
+      <Helmet title={siteMetadata.title} titleTemplate="" />
+      <h1 className="visually-hidden">Welcome</h1>
+      <Hero />
+      <About />
+      <Testimonial />
+      <Faq />
+      <Signup />
+    </Layout>
+  );
+};
 
 export default IndexPage;
