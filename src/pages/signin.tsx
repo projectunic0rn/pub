f3a6c118a9944954767b7f5e0f5107f611764298
aaron2212 <33976953+aaron2212@users.noreply.github.com>
2019-09-02 18:13:57 +0000

    Front end login page (#52)
    * added login page with email and password fields
    
    * added-links-and-sign-in-button
    
    * Added Sign In link to the nav menu. Created the sign up page
    
    * Addressed PR comments
    
    * Update layout.tsx

---
+++ src/pages/signin.tsx
@@ -0,0 +1,78 @@
+import { Link } from 'gatsby';
+import * as React from 'react';
+
+import styled from '@styled-components';
+import { Layout, Seo } from '@components/shared';
+import CtaButton from '@components/index-page/cta-button';
+import { useSiteMetadata } from '@hooks';
+import Form, {
+  FormLabel,
+  FormInput,
+  LinkWrapper,
+  ButtonWrapper,
+} from '@components/shared/form';
+import { useState } from 'react';
+
+const Wrapper = styled.section`
+  background-color: ${({ theme }) => theme.colors.section};
+
+  padding: ${({ theme }) => theme.boxes.padding.section.smallTop};
+
+  @media screen and(max-width: ${({ theme }) => theme.sizes.width.medium}) {
+    flex-direction: column;
+  }
+
+  @media screen and(max-width: ${({ theme }) => theme.sizes.width.small}) {
+    padding: ${({ theme }) => theme.boxes.padding.section.small};
+  }
+`;
+
+const SignInPage: React.FC = () => {
+  const siteMetadata = useSiteMetadata();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <Layout>
+      <Seo
+        title={`Sign In`}
+        description={`Sign In page for ${siteMetadata.title} website`}
+        urlSlug="signin/"
+      />
+      <Wrapper>
+        <Form heading={`Sign In To ${siteMetadata.title}`}>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <FormInput
+            name="email"
+            type="email"
+            placeholder="unicorn@projectunicorn.net"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <FormLabel htmlFor="password">Confirm Password</FormLabel>
+          <FormInput
+            name="password"
+            type="password"
+            placeholder="Your Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <LinkWrapper>
+            <Link to="/forgotpassword/">Forgot password?</Link>
+          </LinkWrapper>
+
+          <LinkWrapper>
+            <Link to="/signup/">New member? Sign Up!</Link>
+          </LinkWrapper>
+
+          <ButtonWrapper>
+            <CtaButton title="Sign In" href="" type="input" content="Sign In" />
+          </ButtonWrapper>
+        </Form>
+      </Wrapper>
+    </Layout>
+  );
+};
+
+export default SignInPage;
---
+++ src/pages/signup.tsx
@@ -0,0 +1,98 @@
+import { Link } from 'gatsby';
+import * as React from 'react';
+
+import styled from '@styled-components';
+import { Layout, Seo } from '@components/shared';
+import CtaButton from '@components/index-page/cta-button';
+import { useSiteMetadata } from '@hooks';
+import Form, {
+  FormLabel,
+  FormInput,
+  LinkWrapper,
+  ButtonWrapper,
+} from '@components/shared/form';
+import { useState } from 'react';
+
+const Wrapper = styled.section`
+  background-color: ${({ theme }) => theme.colors.section};
+
+  padding: ${({ theme }) => theme.boxes.padding.section.smallTop};
+
+  @media screen and(max-width: ${({ theme }) => theme.sizes.width.medium}) {
+    flex-direction: column;
+  }
+
+  @media screen and(max-width: ${({ theme }) => theme.sizes.width.small}) {
+    padding: ${({ theme }) => theme.boxes.padding.section.small};
+  }
+`;
+
+const SignUpPage: React.FC = () => {
+  const siteMetadata = useSiteMetadata();
+
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password1, setPassword1] = useState('');
+  const [password2, setPassword2] = useState('');
+
+  return (
+    <Layout>
+      <Seo
+        title={`Sign Up`}
+        description={`Sign Up page for ${siteMetadata.title} website`}
+        urlSlug="signup/"
+      />
+      <Wrapper>
+        <Form heading={`Sign Up To Join ${siteMetadata.title}`}>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <FormInput
+            name="email"
+            type="email"
+            placeholder="unicorn@projectunicorn.net"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <FormLabel htmlFor="username">Username</FormLabel>
+          <FormInput
+            name="username"
+            type="text"
+            placeholder="unicorn21"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <FormInput
+            name="password1"
+            type="password"
+            placeholder="Your Password"
+            onChange={(e) => setPassword1(e.target.value)}
+          />
+
+          <FormLabel htmlFor="password">Confirm Password</FormLabel>
+          <FormInput
+            name="password2"
+            type="password"
+            placeholder="Confirm Your Password"
+            onChange={(e) => setPassword2(e.target.value)}
+          />
+
+          <LinkWrapper>
+            <Link to="/signin">Sign In</Link>
+          </LinkWrapper>
+
+          <ButtonWrapper>
+            <CtaButton
+              title="Sign Up"
+              href=""
+              type="input"
+              content="Sign Up"
+              onClick={(e) => e.preventDefault()}
+            />
+          </ButtonWrapper>
+        </Form>
+      </Wrapper>
+    </Layout>
+  );
+};
+
+export default SignUpPage;
