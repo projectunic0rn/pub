3873ee8fa7fe5bae32083b5358fbde4ebdf5e906
Roy Moran <roy_moran1993@yahoo.com>
2019-09-02 13:03:19 -0500

    Update layout.tsx

--- src/components/shared/layout.tsx
+++ src/components/shared/layout.tsx
@@ -30,12 +30,6 @@ const Layout: React.FC<LayoutProps> = ({ children, navLinks = [] }) => {
         href: `//github.com/${siteMetadata.social.github}`,
         title: `${siteMetadata.title} GitHub organization`,
       },
-      {
-        content: 'Sign In',
-        external: false,
-        href: '/signin',
-        title: `${siteMetadata.title} sign in`,
-      },
     ];
   }
 
