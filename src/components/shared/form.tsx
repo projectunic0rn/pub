9ce42034b98d8395d4a811e2518a19af42a3c763
Aaron Garton <atgarton18@gmail.com>
2019-09-11 17:40:13 +0200

    Created the create project form

--- src/components/shared/form.tsx
+++ src/components/shared/form.tsx
@@ -16,13 +16,17 @@ const FormLabel = styled.label`
 const FormInput = styled.input`
   max-width: 400px;
   padding: 0.425em;
-  border-radius: 0;
-  box-shadow: none;
-  border: 1px solid gray;
+  border-radius: 3px;
+  box-shadow: 0 0 1px gray;
+  border: 1px solid lightgray;
 
   :focus {
     border-color: black;
   }
+`;
+
+const FormHint = styled.small`
+  color: gray;
 `;
 
 const LinkWrapper = styled.div`
@@ -36,11 +40,11 @@ const ButtonWrapper = styled.div`
   padding: 1.825em 0.125em;
 `;
 
-interface Props {
-  heading: string;
+interface FormProps {
+  heading?: string;
 }
 
-const Form: React.FC<Props> = ({ heading, children }) => {
+const Form: React.FC<FormProps> = ({ heading, children }) => {
   return (
     <FormElement>
       <Heading>{heading}</Heading>
@@ -49,5 +53,102 @@ const Form: React.FC<Props> = ({ heading, children }) => {
   );
 };
 
-export { FormLabel, FormInput, LinkWrapper, ButtonWrapper };
+interface FormSelectInputProps {
+  options: string[];
+  name?: string;
+  onChange?: Function;
+}
+
+const FormSelectInput: React.FC<FormSelectInputProps> = ({
+  options,
+  name,
+  onChange,
+}) => {
+  const selectOptions = options.map((option: string) => (
+    <option key={option}>{option}</option>
+  ));
+
+  const SelectInput = styled.select`
+    max-width: 400px;
+    padding: 0.425em;
+    border-radius: 3px;
+    box-shadow: 0 0 1px gray;
+    border: 1px solid lightgray;
+    appearance: none;
+
+    :focus {
+      border-color: black;
+    }
+
+    background: url(http://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/br_down.png)
+      no-repeat right white;
+    background-position-x: 360px;
+  `;
+
+  return (
+    <SelectInput
+      name={name}
+      onChange={onChange !== undefined ? (e) => onChange(e) : () => {}}
+    >
+      {selectOptions}
+    </SelectInput>
+  );
+};
+
+interface FormTextAreaProps {
+  onChange: Function;
+  value: string;
+  displayCharCount: boolean;
+  maxCharCount: number;
+}
+
+const FormTextArea: React.FC<FormTextAreaProps> = ({
+  onChange,
+  value,
+  displayCharCount,
+  maxCharCount,
+}) => {
+  const TextArea = styled.textarea`
+    width: 400px;
+    padding: 0.425em;
+    border-radius: 3px;
+    box-shadow: 0 0 1px gray;
+    border: 1px solid lightgray;
+
+    :focus {
+      border-color: black;
+    }
+  `;
+
+  const FormTextAreaHint = styled.small`
+    color: black;
+    position: relative;
+    left: -55px;
+    bottom: 5px;
+  `;
+
+  return (
+    <div>
+      <TextArea
+        onChange={onChange !== undefined ? (e) => onChange(e) : () => {}}
+        value={value}
+      />
+      {displayCharCount && (
+        <FormTextAreaHint>
+          {value.length}/{maxCharCount}
+        </FormTextAreaHint>
+      )}
+    </div>
+  );
+};
+
+export {
+  FormLabel,
+  FormInput,
+  FormTextArea,
+  FormSelectInput,
+  FormHint,
+  LinkWrapper,
+  ButtonWrapper,
+};
 export default Form;
