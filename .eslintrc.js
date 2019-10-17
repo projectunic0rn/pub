afe4f7bc26135722ce355853744211ef75d2f6e0
Roy Moran <roy_moran1993@yahoo.com>
2019-10-17 00:24:27 -0500

    add no-console to eslint and set to error

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'no-console': 'error',
   },
   overrides: [
     {
