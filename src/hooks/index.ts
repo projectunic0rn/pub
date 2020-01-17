ac695049dab8c58eeb94627bdd810674bfe09a10
Rodger Jordas <rmjordas@gmail.com>
2020-01-18 06:10:56 +0800

    Revert "Add useScrollPosition custom React hook"
    This reverts commit 17ca9bfcf21d8098a8e2f99e17e299ce7db74a25.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,3 @@
 export * from './use-default-avatar-image';
 export * from './use-default-post-image';
-export * from './use-scroll-position';
 export * from './use-site-metadata';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
-export * from './browser-utils';
 export * from './number-utils';
 export * from './string-utils';
 export * from './url-utils';
