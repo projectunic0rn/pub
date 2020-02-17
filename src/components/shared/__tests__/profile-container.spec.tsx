a1a76e06bc1838af95f92c85598faf0f7c842a3f
Niket Malik <niketmalik@gmail.com>
2020-02-17 18:20:12 +0530

    [BUG] profile-container breaking due to improper checks. (#167)
    * Fixed user.technologies error check.
    
    * improve profile test coverage
    
    * adjust threshold
    
    * adjust test name
    
    * resolve lint error
    
    Co-authored-by: Roy Moran <roy_moran1993@yahoo.com>

---
+++ src/components/shared/__tests__/profile-container.spec.tsx
@@ -0,0 +1,74 @@
+import { ProfileContainer } from '@components/shared/containers';
+import { render, waitForElement } from '@testing-library/react';
+import React from 'react';
+import { MockThemeProvider } from '@mocks';
+import { user } from '@mocks/responses';
+
+describe('profile container', () => {
+  test('displays username returned by api', async () => {
+    // Arrange
+    const apiUsernameData = user.data.username;
+    const { getByText } = render(
+      <MockThemeProvider>
+        <ProfileContainer path="profile/1" />
+      </MockThemeProvider>,
+    );
+    // Act
+    const profileUsername = await waitForElement(() =>
+      getByText(apiUsernameData),
+    );
+    // Assert
+    expect(profileUsername).toBeVisible();
+    expect(profileUsername).toHaveTextContent(apiUsernameData);
+  });
+
+  test('displays all user technologies returned by api', async () => {
+    // Arrange
+    const apiTechnologiesData = user.data.technologies;
+
+    const { getAllByTestId } = render(
+      <MockThemeProvider>
+        <ProfileContainer path="profile/1" />
+      </MockThemeProvider>,
+    );
+    // Act
+    const profileTechnology = await waitForElement(() =>
+      getAllByTestId('technology'),
+    );
+    // Assert
+    expect(profileTechnology.length).toBe(apiTechnologiesData.length);
+  });
+
+  test('displays user bio returned by api', async () => {
+    // Arrange
+    const apiBioData = user.data.bio;
+    const { getByText } = render(
+      <MockThemeProvider>
+        <ProfileContainer path="profile/1" />
+      </MockThemeProvider>,
+    );
+    // Act
+    const profileBio = await waitForElement(() => getByText(apiBioData));
+    // Assert
+    expect(profileBio).toBeVisible();
+    expect(profileBio).toHaveTextContent(apiBioData);
+  });
+
+  test('displays user profile picture', async () => {
+    // Arrange
+    const altText = 'Profile Picture';
+    const { getByAltText } = render(
+      <MockThemeProvider>
+        <ProfileContainer path="profile/1" />
+      </MockThemeProvider>,
+    );
+    // Act
+    const profilePictureAltText = await waitForElement(() =>
+      getByAltText(altText),
+    );
+
+    // Assert
+    expect(profilePictureAltText).toBeVisible();
+    expect(profilePictureAltText).toBeDefined();
+  });
+});
