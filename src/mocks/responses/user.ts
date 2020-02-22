a340ae1698c48b4057232148d4da3adbae5e4d13
Roy Moran <roy_moran1993@yahoo.com>
2020-02-22 02:24:48 -0600

    adjust user type to reflect api response (#168)

--- src/mocks/responses/user.ts
+++ src/mocks/responses/user.ts
@@ -9,16 +9,46 @@ export const user: ApiResponse<User> = {
     bio:
       'I am a specialized unicorn developer who is well experienced in HTML, CSS, Python, PHP, React, JavaScript, and TypeScript. I am open to learning new tech and languages.',
     technologies: [
-      'HTML',
-      'CSS',
-      'JavaScript',
-      'Typescript',
-      'React',
-      'Python',
-      'PHP',
-      'Java',
-      'VueJS',
-      'Angular',
+      {
+        name: 'HTML',
+        userId: '04200724-b54c-4fc1-84c7-187475f2f68d',
+      },
+      {
+        name: 'CSS',
+        userId: '04200724-b54c-4fc1-84c7-187475f2f68d',
+      },
+      {
+        name: 'JavaScript',
+        userId: '04200724-b54c-4fc1-84c7-187475f2f68d',
+      },
+      {
+        name: 'Typescript',
+        userId: '04200724-b54c-4fc1-84c7-187475f2f68d',
+      },
+      {
+        name: 'React',
+        userId: '04200724-b54c-4fc1-84c7-187475f2f68d',
+      },
+      {
+        name: 'Python',
+        userId: '04200724-b54c-4fc1-84c7-187475f2f68d',
+      },
+      {
+        name: 'PHP',
+        userId: '04200724-b54c-4fc1-84c7-187475f2f68d',
+      },
+      {
+        name: 'Java',
+        userId: '04200724-b54c-4fc1-84c7-187475f2f68d',
+      },
+      {
+        name: 'VueJS',
+        userId: '04200724-b54c-4fc1-84c7-187475f2f68d',
+      },
+      {
+        name: 'Angular',
+        userId: '04200724-b54c-4fc1-84c7-187475f2f68d',
+      },
     ],
     projects: [],
   },
