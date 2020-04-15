e848a3b392f3c741b18ece8d9a73d500fcbe48d6
Rodger Jordas <rmjordas@gmail.com>
2020-04-15 08:11:11 +0800

    Update dependencies (#173)
    * Update dependencies
    
    * Fix minor formatting issue
    
    This change was caused by the Prettier format command.
    
    * Update package manifest
    
    * Update Lint Staged configuration
    
    * Use named import for react-helment
    
    Default import is not allowed for react-helmet@6
    
    * Fix broken tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 
 import { Hero, About, Signup, Faq, Testimonial } from '@components/index-page';
 import { Layout } from '@components/shared';
