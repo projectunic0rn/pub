308de6418263a220b56c408f8de2d9388426461a
Roy Moran <roy_moran1993@yahoo.com>
2020-06-04 20:28:57 -0500

    add description that more closely describes community aspiration (#213)
    * add description that more closely describes community aspiration
    
    * modify description message

--- __mocks__/gatsby.js
+++ __mocks__/gatsby.js
@@ -34,7 +34,7 @@ const mockUseStaticQueryImplementation = () => {
         tag: 'Build something awesome.',
         /** The description of the website. */
         description:
-          'Project Unicorn is a virtual co-working space of software developers around the world working together to create and deploy meaningful software.',
+          'Project Unicorn is a community where developers discover meaningful software projects and help each other build and ship them.',
         /** The url of the website. */
         siteUrl: 'http://localhost:8000',
         logo: 'src/images/logo.png',
