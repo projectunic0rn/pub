8affa4c9682221a8308e4c82f307bd8ad36477c3
Roy Moran <roy_moran1993@yahoo.com>
2020-08-30 04:28:30 -0500

    add workspace page button, add placeholder multi-tab-menu component (#251)
    * add workspace page button, add placeholder multi-tab-menu component
    
    * modify project workspace test

--- src/components/projects/project-workspace/__tests__/project-workspace.spec.tsx
+++ src/components/projects/project-workspace/__tests__/project-workspace.spec.tsx
@@ -21,16 +21,21 @@ describe('project workspace page tests', () => {
 
   test('get project details returns successfully', async () => {
     // Arrange
-    const { getByText } = render(
+    const { findByText } = render(
       <MockThemeProvider>
         <ProjectWorkspace projectId="08d6c5e7-618f-0a0b-f6bb-b8600e4e4c56" />
       </MockThemeProvider>,
     );
     // Act
-    const text = await waitFor(() => getByText('Join Team, Become Fan'));
+    const joinButton = await findByText('Join Team');
+    const fanButton = await findByText('Become Fan');
     // Assert
-    expect(text).toBeDefined();
-    expect(text).toBeInTheDocument();
-    expect(text).toBeVisible();
+    expect(joinButton).toBeDefined();
+    expect(joinButton).toBeInTheDocument();
+    expect(joinButton).toBeVisible();
+
+    expect(fanButton).toBeDefined();
+    expect(fanButton).toBeInTheDocument();
+    expect(fanButton).toBeVisible();
   });
 });
