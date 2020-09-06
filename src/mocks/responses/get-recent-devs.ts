58f463f0bb45a02522ff378d6e7db8b485fb9a04
Roy Moran <romoran1@outlook.com>
2020-09-06 03:53:14 -0500

    modify recent devs mock response

--- src/mocks/responses/get-recent-devs.ts
+++ src/mocks/responses/get-recent-devs.ts
@@ -1,6 +1,11 @@
 import { ApiResponse } from '@api';
 import { RecentDev } from '@api/types/recent-dev';
 
+const today = new Date();
+const oneDayAgo = new Date(today);
+
+oneDayAgo.setDate(oneDayAgo.getDate() - 1);
+
 export const getRecentDevs: ApiResponse<RecentDev[]> = {
   ok: true,
   data: [
@@ -8,13 +13,13 @@ export const getRecentDevs: ApiResponse<RecentDev[]> = {
       id: '08d6c5e7-6100-c770-61c3-834f6474a77b',
       bio:
         'Hi. I’m Dan. I’ve been working as a programmer for ~15 years now. Python, ML, NLP, server-side...',
-      updatedAt: new Date(),
+      updatedAt: today,
     },
     {
       id: '08d6c5e7-6100-c770-61c3-834f6474a77c',
       bio:
         'Hi. I’m Dan. I’ve been working as a programmer for ~15 years now. Python, ML, NLP, server-side...',
-      updatedAt: new Date(2020, 8, 4),
+      updatedAt: oneDayAgo,
     },
     {
       id: '08d6c5e7-6100-c770-61c3-834f6474a77d',
