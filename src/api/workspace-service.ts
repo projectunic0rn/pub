8a296d872bbb2de22e8ffdacdf88bc1aa281feae
Roy Moran <romoran1@outlook.com>
2020-10-29 02:48:44 -0500

    left align workspace content

--- src/api/workspace-service.ts
+++ src/api/workspace-service.ts
@@ -16,10 +16,7 @@ export class WorkspaceService {
   }
 
   public async getWorkspaceInfo(workspaceType: string): Promise<WorkspaceInfo> {
-    const apiEndpoint = this.apiEndpoint.replace(
-      'workspaceType',
-      workspaceType,
-    );
+    const apiEndpoint = this.resolveWorkspaceEndpoint(workspaceType);
     const endpoint = `${apiEndpoint}/info`;
     this.profiler.setReportInfo(endpoint, 'getWorkspaceInfo');
     this.profiler.startTimeRecord();
@@ -32,15 +29,30 @@ export class WorkspaceService {
     workspaceType: string,
     workspaceAppAuth: WorkspaceAppAuth,
   ) {
-    const apiEndpoint = this.apiEndpoint.replace(
-      'workspaceType',
-      workspaceType,
-    );
+    const apiEndpoint = this.resolveWorkspaceEndpoint(workspaceType);
     const endpoint = `${apiEndpoint}/finish_auth?code=${workspaceAppAuth.code}&project=${workspaceAppAuth.project}&workspace=${workspaceAppAuth.workspace}&permissions=${workspaceAppAuth.permissions}`;
     this.profiler.setReportInfo(endpoint, 'finishAuth');
     this.profiler.startTimeRecord();
     await HttpClient.post(endpoint, this.headers, workspaceAppAuth);
     this.profiler.endTimeRecord();
     return;
+  }
+
+  private resolveWorkspaceEndpoint(workspaceType: string) {
+    if (
+      process.env.GATSBY_USE_MOCK === '0' &&
+      process.env.GATSBY_ACTIVE_ENV === 'development'
+    ) {
+      switch (workspaceType) {
+        case 'slack':
+          return this.apiEndpoint.replace('workspaceType', '8003');
+        case 'discord':
+          return this.apiEndpoint.replace('workspaceType', '8002');
+        default:
+          return this.apiEndpoint.replace('workspaceType', '8003');
+      }
+    } else {
+      return this.apiEndpoint.replace('workspaceType', workspaceType);
+    }
   }
 }
