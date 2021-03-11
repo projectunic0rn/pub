bdae607141b139c7e5440d5ecfeb5f526f7c22c3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-11 07:29:14 +0000

    Deploy projectunic0rn/pub to github.com/projectunic0rn/pub.git:gh-pages

--- sw.js
+++ sw.js
@@ -39,11 +39,11 @@ self.__precacheManifest = [
     "url": "styles-ea1477eb628020fd2ad0.js"
   },
   {
-    "url": "app-73a2b372fe2f4c2e38d4.js"
+    "url": "app-24ee974ed8b26294cf02.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f2491726e0393bc4d6c3ef54c0e22ee0"
+    "revision": "e9b9bd95594977441d9e10104544846e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-975bfb9556d1a5ee781e.js"
@@ -146,7 +146,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-73a2b372fe2f4c2e38d4.js`))) {
+  if (!resources || !(await caches.match(`/app-24ee974ed8b26294cf02.js`))) {
     return await fetch(event.request)
   }
 
